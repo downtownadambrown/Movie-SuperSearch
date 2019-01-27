c24bd9a02461f5c74fb543ade7721b8ca89db87e
Adam Brown <downtownadambrown@gmail.com>
2019-01-27 12:18:29 -0500

    configuring mongoose for mongodb

--- routes.js
+++ routes.js
@@ -1,6 +1,11 @@
-module.exports = function(app) {
-    app.get('/api/search', function(req, res){
+const mongoose = require("mongoose");
+const url = 'mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge';
+const db = mongoose.connect(url, { useNewUrlParser: true });
 
+module.exports = (app) => {
+    app.get('/api/search', function(req, res){
+        db.collection('Titles').find({}).toArray().then((matches) => res.send(matches));
+        //res.send({ express: "connected to mongo!" });
     });
 
     app.get('/test', function(req, res) {
