26b25740c1cd502c9ef515920ee003bbcb4e03c2
Adam Brown <downtownadambrown@gmail.com>
2019-01-27 13:28:39 -0500

    rolled back mongo version to 2.2.x because of apparent changes in mongoclient/issues with collections

--- routes.js
+++ routes.js
@@ -1,15 +1,18 @@
-const mongoose = require("mongoose");
 const url = 'mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge';
-const db = mongoose.connect(url, { useNewUrlParser: true });
+// const db = mongoose.connect(url, { useNewUrlParser: true });
+const mongoClient = require('mongodb').MongoClient;
 
-module.exports = (app) => {
+module.exports = async (app) => {
+    const db = await mongoClient.connect(url).catch(console.error);
+
     app.get('/api/search', function(req, res){
+        const regexQuery = '/*/';
         db.collection('Titles').find({}).toArray().then((matches) => res.send(matches));
         //res.send({ express: "connected to mongo!" });
     });
 
     app.get('/test', function(req, res) {
-        console.log('route \'test/\' hit with: ', req.body);
+        console.log('route test/ hit with: ', req.body);
         res.send({ express: "Connected to React!" });
     });
 };
