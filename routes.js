7f4c38ba02c725607f806769555b12128f24730d
Adam Brown <downtownadambrown@gmail.com>
2019-02-04 16:19:24 -0500

    Linted routes.js

--- routes.js
+++ routes.js
@@ -2,15 +2,15 @@ const url = 'mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge
 const mongoClient = require('mongodb').MongoClient;
 
 module.exports = async (app) => {
-    const db = await mongoClient.connect(url).catch(console.error);
+  const db = await mongoClient.connect(url).catch((err) => { console.log(err); });
 
-    app.get('/api/search', function(req, res){
-        const regexQuery = new RegExp(req.query.q, 'i');
-        db.collection('Titles')
-            .find({ "TitleName" : regexQuery })
-            .toArray()
-            .then((matches) => {
-                res.send(matches);
-            });
-    });
+  app.get('/api/search', (req, res) => {
+    const regexQuery = new RegExp(req.query.q, 'i');
+    db.collection('Titles')
+      .find({ TitleName: regexQuery })
+      .toArray()
+      .then((matches) => {
+        res.send(matches);
+      });
+  });
 };
