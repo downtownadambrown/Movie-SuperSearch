4065794cc9ae4133e601c367ff7006609620db0b
Adam Brown <downtownadambrown@gmail.com>
2019-02-04 22:53:10 -0500

    heroku changes

--- server.js
+++ server.js
@@ -5,16 +5,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 const bunyan = require('express-bunyan-logger');
+const env = process.env.NODE_ENV || 'dev';
 
 // Configure Express to handle requests
 app.use(bunyan.errorLogger());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, './client/public')));
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Import all routes
-require('./routes')(app);
+// Import db routes
+require('./db_routes')(app);
+
+app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname, 'client/build/index.html'));
+});
 
 // Listen on a port for requests
 app.listen(port, () => console.log(`Listening on port ${port}`));
