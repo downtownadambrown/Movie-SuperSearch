4b4ecb8ec8fca769da1d3a334e16aa9f95490486
Adam Brown <downtownadambrown@gmail.com>
2019-02-04 23:17:28 -0500

    build updt

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
         });
 
         const body = await response.json;
-        console.log(response);
+        console.log(body);
 
         if (response.status !== 200) {
             throw Error(body.message)
