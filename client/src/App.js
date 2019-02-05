5cf60ed9a3c4a770d8383c20015a29e22bad9631
Adam Brown <downtownadambrown@gmail.com>
2019-02-04 23:34:05 -0500

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -14,23 +14,20 @@ class App extends Component {
         this.showDetails = this.showDetails.bind(this);
     }
 
-    queryRequest = async (query) => {
+    queryRequest = (query) => {
         const searchPath = `/api/search?q=${query}`;
-        const response = await fetch(searchPath, {
+        fetch(searchPath, {
             method: "GET",
             mode: "no-cors",
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json" }
+        }).then((response) => {
+            console.log(response);
+            return response;
+        }).catch((err) => {
+            throw new Error(err);
         });
-
-        const body = await response.json;
-        console.log(body);
-
-        if (response.status !== 200) {
-            throw Error(body.message)
-        }
-        return body;
     };
 
     updateState(event) {
